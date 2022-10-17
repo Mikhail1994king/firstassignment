02f87959a5b5bfcb12235e3aa10d0fd7d0cbaefc
米哈伊尔 <mihayiermihayier@mihayiers-MacBook-Air.local>
2022-10-17 16:14:54 +0800

    fdfjdf

--- data_collection_weibo.py
+++ data_collection_weibo.py
@@ -45,7 +45,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-notifications");
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-PATH = ("./chromedriver")
+PATH = ("/Users/mihayiermihayier/Desktop/twNLP-app/firstassignment/firstassignment/chromedriver")
 driver = webdriver.Chrome(executable_path=PATH, options = chrome_options)
 
 
