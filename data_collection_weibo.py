85c116827e96b949ae8cc49bd8bb3a548adb86c7
米哈伊尔 <mihayiermihayier@mihayiers-MacBook-Air.local>
2022-10-17 15:54:53 +0800

    fdjfh

--- data_collection_weibo.py
+++ data_collection_weibo.py
@@ -45,7 +45,8 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-notifications");
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(executable_path=(r"chromedriver"), options = chrome_options)
+PATH = ("./chromedriver")
+driver = webdriver.Chrome(executable_path=PATH, options = chrome_options)
 
 
 
